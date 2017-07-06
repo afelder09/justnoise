23579f1d00af894a277c5b39aabe619bba49c8fe
Anson Felder <afelder09@gmail.com>
2017-07-06 14:14:54 -0400

    Changed database

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const hbs = require('express-handlebars')
 const mongoose = require('mongoose')
 
 // Connect to our Mongo database, using Mongoose and include our models
-mongoose.connect('mongodb://admin:password1$@dopeondope-shard-00-00-32pjn.mongodb.net:27017,dopeondope-shard-00-01-32pjn.mongodb.net:27017,dopeondope-shard-00-02-32pjn.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=DopeOnDope-shard-0&authSource=admin')
+mongoose.connect('mongodb://admin:Password1$@cluster0-shard-00-00-32pjn.mongodb.net:27017,cluster0-shard-00-01-32pjn.mongodb.net:27017,cluster0-shard-00-02-32pjn.mongodb.net:27017/justnoise?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 
 // Require our models
 const Contributor = require('./models/contributor.js')
@@ -38,6 +38,6 @@ app.use('/group', groupRouter)
 app.use('/post', postRouter)
 
 // Listen on port 3000
-app.listen( 3000, () => {
-  console.log( 'listening on 3000' )
+app.listen( 3002, () => {
+  console.log( 'listening on 3002' )
 })
